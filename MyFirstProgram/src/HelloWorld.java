ea7383b4d54ff45f01c14e91724d374cb087fa52
vandana1212 <NIST9BVandana@gmail.com>
2017-10-22 15:52:25 +0530

    This is my first commit

---
+++ MyFirstProgram/src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+	
+	public static void fun()
+	{
+		System.out.println("This will be fun");
+	}
+	
+	public static void main(String []args)
+	{
+		System.out.println("My first program");
+		fun();
+	}
+}
